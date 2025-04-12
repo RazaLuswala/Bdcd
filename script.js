bbce31353fae6599da2a775eb5044f0754270b74
RazaLuswala <111947691+RazaLuswala@users.noreply.github.com>
2025-04-13 04:45:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,13 +13,15 @@ function updateCountdown() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    countdown.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s until her birthday!`;
+    countdown.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s until this beautiful person's birthday!`;
 }
 
 setInterval(updateCountdown, 1000);
 
 // Daily messages (from April 15 to May 4)
 const messages = {
+    "4/13": "Every thing turns to gold by your touch !",
+    "4/14": "No girl can outshine you, those moons get light from you !",
     "4/15": "You're one of the kindest souls I've ever met.",
     "4/16": "Your presence makes ordinary days special.",
     "4/17": "Every chat with you brightens my mood.",
